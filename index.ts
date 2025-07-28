bffa31939e7988b3c6b68b470f83fe9d8b22be0f
Johnatan Botero <johnatan@jbotero.com>
2025-07-27 22:27:47 -0500

    refactor: Remove Apollo client and cache files, update exports in index and provider

--- index.ts
+++ index.ts
@@ -1,40 +1,31 @@
 // Hook-based client (for React components)
-export { useViewer, viewerFragment, GET_VIEWER_QUERY } from "./hooks/useViewer";
 export {
-  useGetPropertiesPublic,
-  useGetProperty,
-  propertiesPublicFragment,
-  GET_PROPERTIES_PUBLIC_QUERY,
+  GET_PROPERTIES_PUBLIC_QUERY, propertiesPublicFragment, useGetPropertiesPublic,
+  useGetProperty
 } from "./hooks/useGetProperties";
+export { GET_VIEWER_QUERY, useViewer, viewerFragment } from "./hooks/useViewer";
 
-export * from "./gql/graphql";
 export { useFragment } from "./gql/fragment-masking";
+export * from "./gql/graphql";
 
 // Generated types and documents
 export type {
-  Viewer,
-  Viewer_FragmentFragment,
-  GetViewerQuery,
-  GetViewerQueryVariables,
-  UpdateViewerMutation,
-  UpdateViewerMutationVariables,
-  InputViewerUpdate,
-  InputViewerCreate,
-  Property,
-  PropertyPublic,
   GetPropertiesPublicQuery,
-  GetPropertiesPublicQueryVariables,
+  GetPropertiesPublicQueryVariables, GetViewerQuery,
+  GetViewerQueryVariables, InputViewerCreate, InputViewerUpdate, Property,
+  PropertyPublic, UpdateViewerMutation,
+  UpdateViewerMutationVariables, Viewer,
+  Viewer_FragmentFragment
 } from "./gql/graphql";
 
 // Export the union type for properties
 export type { PropertyUnion } from "./hooks/useGetProperties";
 
 // Direct client (for server-side or non-React usage)
-export { client } from "./client";
-export { default as apolloClient } from "./client";
+export { default as apolloClient, client } from "../../apollo/client";
 
 // Cache
-export { cache } from "./apolloCache";
+export { cache } from "../../apollo/apolloCache";
 
 // Provider component
-export { GraphQLProvider } from "./provider";
+export { GraphQLProvider } from "../../apollo/provider";
